53e2ffe78ca5c1995aa4e1d8d8e68a3d6f2565cd
kamiada <kamiada528@gmail.com>
2021-03-03 15:51:13 +0000

    still fighting with p5

--- sketch.js
+++ sketch.js
@@ -1,10 +1,20 @@
+let squaresX = 80;
+let squaresY = 80;
+
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
   background(68, 194, 253);
-  textSize(50)
-  text('DATA', 400, 400)
-  textAlign(CENTER, BASELINE);
+  noStroke();
+  for (let i = 0; i < squaresX.length; i++) {
+    rect(200, 200, 50, 50);
+    console.log(i);
+  }
+  for (var rectNum = 0; rectNum <= 8; rectNum += 1) {
+    fill(rectNum * 32);
+    rect(mouseX + rectNum * 20, mouseY, 20, 60);
+  }
+
 }
